563651aec344f4bcbade8982da3763223b08dacf
QC2168 <huangxinhong2125@gmail.com>
2023-11-09 15:27:31 +0800

    feat: external node modules

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { type PluginOption, build } from 'vite'
-import { generateCfg } from './utils'
+import { build } from 'vite'
+import { generateCfg, withExternalBuiltins } from './utils'
 import type { OptionsType } from './types'
 
-export default function utools(options: OptionsType | OptionsType[]): PluginOption[] {
+export default function utools(options: OptionsType | OptionsType[]) {
   const optionsArr = Array.isArray(options) ? options : [options]
   return [{
     name: 'vite-plugin-utools',
     apply: 'serve',
     buildStart() {
       for (const options of optionsArr)
-        build(generateCfg(options))
+        build(withExternalBuiltins(generateCfg(options)))
     },
   }, {
     name: 'vite-plugin-utools',
     apply: 'build',
     closeBundle() {
       for (const options of optionsArr)
-        build(generateCfg(options))
+        build(withExternalBuiltins(generateCfg(options)))
     },
   }]
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,39 @@
+import { builtinModules } from 'node:module'
 import type { InlineConfig } from 'vite'
 import { mergeConfig } from 'vite'
 import type { OptionsType } from './types'
+
+export function withExternalBuiltins(config: InlineConfig) {
+  const builtins = builtinModules.filter(e => !e.startsWith('_'))
+  builtins.push('electron', ...builtins.map(m => `node:${m}`))
+
+  config.build ??= {}
+  config.build.rollupOptions ??= {}
+
+  let external = config.build.rollupOptions.external
+  if (
+    Array.isArray(external)
+    || typeof external === 'string'
+    || external instanceof RegExp
+  ) {
+    external = builtins.concat(external as string[])
+  }
+  else if (typeof external === 'function') {
+    const original = external
+    external = function (source, importer, isResolved) {
+      if (builtins.includes(source))
+        return true
+
+      return original(source, importer, isResolved)
+    }
+  }
+  else {
+    external = builtins
+  }
+  config.build.rollupOptions.external = external
+
+  return config
+}
 
 export function generateCfg({ entry, vite = {} }: OptionsType) {
   const defaultCfg = {
