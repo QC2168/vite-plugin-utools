f23ba49c479d4d58b916ec6e85c4fdb8b2423c26
QC2168 <tom.huang.2002@gmail.com>
2024-12-18 10:54:56 +0800

    fix: deps cannot be installed

--- src/install/getDependencies.ts
+++ src/install/getDependencies.ts
@@ -6,6 +6,17 @@ import fs from 'fs-extra'
 // github issues https://github.com/babel/babel/issues/13855
 const traverse = (_traverse as any).default
 
+// 筛选包名
+// issues https://github.com/QC2168/vite-plugin-utools/issues/1
+function extractContentBasedOnConditions(str: string) {
+  // 判断包名是否作者开头
+  if (str.startsWith('@'))
+    return str
+  // 返回第一个/前面的包名
+  const slashIndex = str.indexOf('/')
+  return slashIndex === -1 ? str : str.substring(0, slashIndex)
+}
+
 export default function getDependencies(filePath: string) {
   const code = fs.readFileSync(filePath, 'utf-8')
   const ast = parser.parse(code, {
@@ -18,7 +29,7 @@ export default function getDependencies(filePath: string) {
   traverse(ast, {
     ImportDeclaration(path: any) {
       const source = path.node.source.value
-      packageNames.push(source)
+      packageNames.push(extractContentBasedOnConditions(source))
     },
     CallExpression(path: any) {
       if (
@@ -27,7 +38,7 @@ export default function getDependencies(filePath: string) {
         && path.node.arguments.length === 1
         && path.node.arguments[0].type === 'StringLiteral'
       ) {
-        packageNames.push(path.node.arguments[0].value)
+        packageNames.push(extractContentBasedOnConditions(path.node.arguments[0].value))
       }
     },
   })
